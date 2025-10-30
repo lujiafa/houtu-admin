8fb1219479e417e5950d6062aafcc639a5c4adb1
jonlu <jonlu@yeahka.com>
2025-10-30 08:27:46 +0800

    优化已知问题

--- mp/src/test/java/com/houtu/mp/UserTests.java
+++ mp/src/test/java/com/houtu/mp/UserTests.java
@@ -2,14 +2,16 @@ package com.houtu.mp;
 
 import com.houtu.mp.module.sys.dao.SysUserDao;
 import com.houtu.mp.module.sys.entity.SysUserEntity;
+import com.houtu.util.common.CodecData;
 import com.houtu.util.crypto.HMacSHAUtils;
-import com.houtu.util.data.HexUtils;
+import com.houtu.util.crypto.type.HmacSHAAlgorithm;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 @SpringBootTest
 public class UserTests {
@@ -22,7 +24,7 @@ public class UserTests {
     @Test
     public void updatePassword() {
         String pwd = "admin123";
-        String encPwd = HexUtils.toHex(HMacSHAUtils.encryptHMacSHA256(pwd.getBytes(), pwd.getBytes()));
+        String encPwd = HMacSHAUtils.hash(CodecData.ascii(pwd), CodecData.ascii(pwd), HmacSHAAlgorithm.HMAC_SHA_1).hex();
         SysUserEntity updateEntity = new SysUserEntity();
         updateEntity.setUserId(1L);
         updateEntity.setPassword(pwdEncoder.encode(encPwd));
@@ -32,4 +34,10 @@ public class UserTests {
 
     }
     // create database sysbase charset=utf8 collate=utf8_general_ci;
+
+
+    public static void main(String[] args) {
+        ArrayList<Object> list = new ArrayList<>();
+        System.out.println(list.parallelStream().allMatch(p -> "wakk".equals(p)));
+    }
 }
